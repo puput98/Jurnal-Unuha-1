40695632e191833f266162cc25b01c26aef482dd
puput98 <119034684+puput98@users.noreply.github.com>
2023-01-27 00:39:05 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+</div>
+    <script src="<?php echo $base_dir ; ?>/asset/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ sidebar.php
@@ -0,0 +1,23 @@
+<div class="side-bar">
+    <div class="list-group menu">
+        <a href="<?php echo $base_url; ?>/index.php" 
+            class="list-group-item list-group-item-action active" aria-current="true"> Dashboard </a>
+
+        <a href="<?php echo $base_url; ?>/mahasiswa/tampil_mahasiswa.php"
+            class="list-group-item list-group-item-action"> Mahasiswa </a>
+
+        <a href="<?php echo $base_url; ?>/dosen/tampil_dosen.php"
+            class="list-group-item list-group-item-action"> Dosen </a>
+
+        <a href="<?php echo $base_url; ?>/prodi/tampil_prodi.php" 
+            class="list-group-item list-group-item-action"> Prodi </a>
+
+        <a class="list-group-item list-group-item-action disabled"> Jurnal </a>
+        
+        <a href="<?php echo $base_url; ?>/tahunajaran/tampil_tahunajaran.php" 
+            class="list-group-item list-group-item-action"> Tahun Ajaran </a>
+
+        <a href="<?php echo $base_url; ?>/matakuliah/tampil_matakuliah.php" 
+        class="list-group-item list-group-item-action"> Matakuliah </a>
+    </div>
+</div>
